17cb7dc61b7a6982da16d11423f61c2ddecfd3cc
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-09-03 11:22:55 -0300

    refactor: adicionando roteamento de paginas template

--- web/src/App.js
+++ web/src/App.js
@@ -1,28 +1,19 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/NavBar";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Home from "./pages";
-// import About from "./pages/about";
-// import Services from "./pages/services";
-// import Contact from "./pages/contact";
-// import SignUp from "./pages/signup";
-import ButtonBlue from "./components/buttonBlue";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {Home, About, Adopt, Login, SignUp} from "./pages";
 
 function App() {
   return (
-    // <Router>
-    //   <Navbar />
-    //   <Switch>
-    //     <Route path="/" exact component={Home} />
-    //     <Route path="/about" component={About} />
-    //     <Route path="/services" component={Services} />
-    //     <Route path="/contact-us" component={Contact} />
-    //     <Route path="/sign-up" component={SignUp} />
-    //   </Switch>
-    // </Router>
-    //<ButtonBlue>Enviar</ButtonBlue>
-    <Navbar></Navbar>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/adopt" component={Adopt} />
+        <Route path="/login" component={Login} />
+        <Route path="/signUp" component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
