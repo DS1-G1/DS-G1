eb77a4d82dd913fe73315920b084a5c70018a9c2
heitormsb <heitormsb@gmail.com>
2021-09-03 15:16:25 -0300

    criado pagina de login

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import iconEmail from "./img/iconEmail.png"
+import iconPassword from "./img/iconPassword.png"
+
+
+function TelaLogin () {
+  return(
+    <div className="containerLogin">
+
+      <div className="splitLeft">
+        <div className="centerlabel loginLabel">Login</div>
+
+        <form className="formContainer">
+
+          <label className="labelsInput">Email</label>
+          <div className="alignIcon">
+            <img src={iconEmail} alt="Email" className="iconEmail" />
+            <input placeholder="seuemail@email.com" type="email" className="inputLogin"></input>
+          </div>
+
+          <label className="labelsInput">Senha</label>
+          <div className="alignIcon">
+            <img src={iconPassword} alt="Password" className="iconPassword" />
+            <input placeholder="Digite sua senha" type="password" className="inputLogin"></input>
+          </div>
+
+          <div className="centerlabel">
+            <label>Não possui cadastro? </label> 
+            <a href={"#"}>Cadastre-se</a>
+          </div>
+
+          <button type="submit" className="loginButton">Login</button>
+        </form>
+      </div>
+
+
+      <div className="splitRight">
+      </div>
+
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TelaLogin />
   );
 }
 
