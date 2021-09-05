43f3e76f9742c13fe382ec4896cf562b5f9e4132
Carlos Rafael <62192072+CarlosZoft@users.noreply.github.com>
2021-09-04 21:23:04 -0300

    Revert "Adicionando component de NavBar"

--- web/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Home} from './home';
-export {default as About} from './about';
-export {default as Adopt} from './adopt';
-export {default as Login} from './login';
-export {default as SignUp} from './signup';
