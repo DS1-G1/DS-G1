8e02a96f65012ff1d31acb17feed20ee7236c53a
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-10-10 18:53:39 -0300

    feat: CRUD user done

--- business/src/services/conta.service.js
+++ business/src/services/conta.service.js
@@ -1,7 +1,15 @@
-const contaModel = require('../models/Conta');
+const contaModel = require("../models/Conta");
 
 export default class ContaService {
   static async create(body) {
     return await contaModel.create(body);
+  }
+  static async update(idConta, body) {
+    return await contaModel.update(body, {
+      where: { id_conta: idConta },
+    });
+  }
+  static async delete(idConta) {
+    return await contaModel.destroy({ where: { id_conta: idConta } });
   }
 }
