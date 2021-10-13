55e0c31fcbb5552ffddfee8f2362ad759b34cd0c
heitormsb <heitormsb@gmail.com>
2021-10-13 12:17:13 -0300

    adicionado listagem dos pets

--- web/src/pages/adminInicial/index.js
+++ web/src/pages/adminInicial/index.js
@@ -4,12 +4,19 @@ import Buttom from "../../components/buttonBlue";
 import * as S from "./style"
 
 
-const PetsData = {
-  ID: "000000",
-  categoria: "Cães",
-  lastModification: "00/00/0000",
-  status: "Não-Adotado"
-}
+const PetsData = [{
+    ID: "000000",
+    categoria: "Cães",
+    lastModification: "00/00/0000",
+    status: "Não-Adotado"
+  },
+  {
+    ID: "000001",
+    categoria: "Gato",
+    lastModification: "00/00/0001",
+    status: "Adotado"
+  }
+]
 
 
 const AdminInicial = () => {
@@ -28,15 +35,17 @@ const AdminInicial = () => {
       <S.LabelPainel>
         Painel Administrativo Geral
       </S.LabelPainel>
+    { PetsData.map( (pets) => 
       <S.ListPets>
-        <S.LabelList>
-          <div>ID: {PetsData.ID}</div>
-          <div>Categoria: {PetsData.categoria}</div>
-          <div>Ultima modificação: {PetsData.lastModification}</div>
-          <div>Status: {PetsData.status}</div>
-          <Buttom height="30px" >Excluir</Buttom>
-        </S.LabelList>
-      </S.ListPets>
+      <S.LabelList>
+        <div>ID: {pets.ID}</div>
+        <div>Categoria: {pets.categoria}</div>
+        <div>Ultima modificação: {pets.lastModification}</div>
+        <div>Status: {pets.status}</div>
+        <Buttom height="30px" >Excluir</Buttom>
+      </S.LabelList>
+    </S.ListPets>
+    ) }
     </S.ListConteiner>
     
     </S.Conteiner>
