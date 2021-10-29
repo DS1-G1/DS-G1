949ce310eb7f3e0adda5e41ab3fefa7fa5e3fda6
CarlosZoft <carlosoftwar@gmail.com>
2021-10-29 15:42:29 -0300

    fix : ultimos ajustes para apresentação
    Co-authored-by: Heitor <heitormsb@gmail.com>
    Co-authored-by: itallo
    <itallo.gravian@gmail.com>
    Co-authored-by: Lucas Soares Barros<lucassb395@gmail.com>

--- web/src/components/PopUp/style.js
+++ web/src/components/PopUp/style.js
@@ -1,67 +1,62 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Conteiner = styled.div` 
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    background-color: rgba(0,0,0,0.2);
+export const Conteiner = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.2);
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
-export const Conteudo = styled.div` 
-    position: relative;
-    padding: 32px;
-    width: 100%;
-    margin: 240px;
-    background-color: #fff;
+export const Conteudo = styled.div`
+  position: relative;
+  padding: 32px;
+  width: 100%;
+  margin: 240px;
+  background-color: #fff;
 
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 50px;
 
-    border-radius: 10px;
-
+  border-radius: 10px;
 `;
 
-
-export const Image = styled.img` 
-    width: 520px;
-
+export const Image = styled.img`
+  width: 520px;
 `;
 
 export const Dados = styled.div`
-    padding: 35px;
+  padding: 35px;
 
-    background: #F7F7F7;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 20px;
+  background: #f7f7f7;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-export const Toppo = styled.div` 
-    display: flex;
-
+export const Toppo = styled.div`
+  display: flex;
 `;
 
 export const TodosDados = styled.div`
-    margin-top: 20px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: repeat(3, 120px);
+  row-gap: 20px;
+`;
 
-
-`
-
-export const Butpms = styled.div` 
-    margin-left: 20px;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+export const Butpms = styled.div`
+  margin-left: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
